c6995b501354ce1d4f6e5201f46d95161a1aba4c
J S <smutek.jakub@seznam.cz>
2024-06-03 16:42:05 +0200

    added option to make compiler ignore auto_ptr in sfml

--- cmake/ExternalTools.cmake
+++ cmake/ExternalTools.cmake
@@ -15,7 +15,7 @@ set(SFML_BUILD_EXAMPLES OFF)
 set(SFML_BUILD_DOC OFF)
 set(SFML_BUILD_NETWORK OFF)
 set(SFML_BUILD_AUDIO ON)
-set(SFML_BUILD_GRAPHICS OFF)
+set(SFML_BUILD_GRAPHICS ON)
 set(SFML_BUILD_WINDOW ON)
 
 FetchContent_GetProperties(sfml)
@@ -23,45 +23,7 @@ if(NOT sfml_POPULATED)
   FetchContent_Populate(sfml)
   add_subdirectory(${sfml_SOURCE_DIR})
 endif()
-
-# include_directories(${SFML_INCLUDE_DIRS})
-# include_directories(SYSTEM ${sfml_SOURCE_DIR}/include)
-# # FetchContent_Declare(
-# #         imgui
-# #         GIT_REPOSITORY https://github.com/glfw/glfw
-# # )
-
-# # FetchContent_GetProperties(imgui)
-# # if(NOT glfw_POPULATED)
-# #     FetchContent_Populate(glfw)
-
-# #     set(GLFW_BUILD_EXAMPLES OFF CACHE INTERNAL "Build the GLFW example programs")
-# #     set(GLFW_BUILD_TESTS OFF CACHE INTERNAL "Build the GLFW test programs")
-# #     set(GLFW_BUILD_DOCS OFF CACHE INTERNAL "Build the GLFW documentation")
-# #     set(GLFW_INSTALL OFF CACHE INTERNAL "Generate installation target")
-
-# #     add_subdirectory(${glfw_SOURCE_DIR} ${glfw_BINARY_DIR})
-# # endif()
-
-
-# # Dear ImGui
-# FetchContent_Declare(
-#   imgui
-#   GIT_REPOSITORY https://github.com/ocornut/imgui
-#   GIT_TAG 35b1148efb839381b84de9290d9caf0b66ad7d03 # 1.82
-# )
-
-# FetchContent_MakeAvailable(imgui)
-
-# # ImGui-SFML
-# FetchContent_Declare(
-#   imgui-sfml
-#   GIT_REPOSITORY https://github.com/SFML/imgui-sfml
-#   GIT_TAG 82dc2033e51b8323857c3ae1cf1f458b3a933c35 # 2.3
-# )
-# FetchContent_MakeAvailable(imgui-sfml)
-# # add_subdirectory(imgui-sfml)
-
+set_target_properties(sfml-audio PROPERTIES CXX_STANDARD 98 CXX_STANDARD_REQUIRED YES CXX_EXTENSIONS NO)
 
 
 # Dear ImGui
