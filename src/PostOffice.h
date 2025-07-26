6118e598f738e7f5f8e15e339902e9389c4ace40
J S <smutek.jakub@seznam.cz>
2025-07-26 20:40:59 +0200

    made event registration at runtime

--- src/PostOffice.h
+++ src/PostOffice.h
@@ -1,65 +1,203 @@
 #pragma once
 
-
 #include <queue>
 #include <unordered_map>
 #include <tuple>
 #include <memory>
 #include <functional>
+#include <typeindex>
 
 #include "GameEvents.h"
 
-template <class T> class PostBox;
+template <class T>
+class PostBox;
+
+using SubscriptionId = int;
+
+template <class MessageT>
+using Callback = std::function<void(const std::deque<MessageT> &)>;
+template <class T>
+using SubscribersT = std::unordered_map<SubscriptionId, Callback<T>>;
+
+class MessageHolderI
+{
+public:
+    virtual void distribute() = 0;
+};
+
+template <class MessageType>
+class MessageHolder : public MessageHolderI
+{
+public:
+    void sendNow(MessageType message)
+    {
+        assert(false); //! TODO will implement
+        // for (auto [id, call_back] : subscribers)
+        // {
+        //     call_back(messages);
+        // }
+    }
+    void send(MessageType message)
+    {
+        messages.push_back(message);
+    }
+
+    int subscribe(Callback<MessageType> subscriber)
+    {
+        int new_id = findNewFreeId();
+        assert(subscribers.count(new_id) == 0);
+        subscribers[new_id] = subscriber;
+        return new_id;
+    }
+
+    void unsubscribe(int id)
+    {
+        subscribers.erase(id);
+    }
+
+    virtual void distribute()
+    {
+        for (auto [id, call_back] : subscribers)
+        {
+            call_back(messages);
+        }
+        messages.clear();
+    };
+
+private:
+    inline int findNewFreeId()
+    {
+        while (subscribers.count(next_id) != 0)
+        {
+            next_id++;
+        }
+        return next_id;
+    }
+
+    int next_id = 0;
+    SubscribersT<MessageType> subscribers;
+    std::deque<MessageType> messages;
+};
 
 class PostOffice
 {
-
-    template <class MessageT>
-    using Callback = std::function<void(const std::deque<MessageT>&)>;
-    template <class T>
-    using SubscribersT = std::unordered_map<int, Callback<T>>;
-
-    using MessageTypes = std::tuple<std::deque<EntityDiedEvent>, std::deque<EntityCreatedEvent>, std::deque<ObjectiveFinishedEvent>>;
-    using SubscriberTypes = std::tuple<SubscribersT<EntityDiedEvent>, SubscribersT<EntityCreatedEvent>, SubscribersT<ObjectiveFinishedEvent>>;
-
-
-    #include <type_traits>
-
-    template <typename T, typename Tuple>
-    struct has_type;
-
-    template <typename T, typename... Us>
-    struct has_type<T, std::tuple<Us...>> : std::disjunction<std::is_same<T, Us>...>
-    {
-    };
 
 public:
     void distributeMessages();
 
     template <class MessageDataT>
     void send(MessageDataT message);
-    
+
+    template <class MessageDataT>
+    void registerEvent();
+
+    template <class ...MessageData>
+    void registerEvents();
+
 private:
     template <class MessageData>
     friend class PostBox;
-    
+
     template <class MessageData>
-    int subscribeTo(PostOffice::Callback<MessageData>& subscriber);
+    bool isRegistered() const;
+
+    template <class MessageData>
+    MessageHolder<MessageData> &getHolder();
+
+    template <class MessageData>
+    int subscribeTo(Callback<MessageData> &subscriber);
 
     template <class MessageDataT>
     void unsubscribe(int id);
 
-    template <class MessageType>
-    void distributeToSubscribers(std::deque<MessageType> &messages);
+    // template <class MessageType>
+    // void distributeToSubscribers(std::deque<MessageType> &messages);
 
-    template <class MessageDataT>
-    int findNewFreeId();
-
-    int next_id = 0;
-
-    MessageTypes m_message_queues;
-    SubscriberTypes m_subscribers;
+    std::unordered_map<std::type_index, std::unique_ptr<MessageHolderI>> m_holders;
 };
 
+// using PostOfficeA = PostOffice<EntityDiedEvent, EntityCreatedEvent, ObjectiveFinishedEvent>;
 
 #include "PostOffice.inl"
+// #pragma once
+
+// #include <queue>
+// #include <unordered_map>
+// #include <tuple>
+// #include <memory>
+// #include <functional>
+
+// #include "GameEvents.h"
+
+// template <class T> class PostBox;
+
+// class MessageHolderI
+// {
+//     // virtual void /
+// };
+
+// template <class MessageType>
+// class MessageHolder : public MessageHolderI
+// {
+//     std::function<void(const std::deque<MessageType>&)> subscribers;
+//     std::deque<MessageType> messages;
+// };
+
+// class PostOffice
+// {
+
+//     template <class MessageT>
+//     using Callback = std::function<void(const std::deque<MessageT>&)>;
+//     template <class T>
+//     using SubscribersT = std::unordered_map<int, Callback<T>>;
+
+//     // using MessageTypes = std::tuple<std::deque<EntityDiedEvent>, std::deque<EntityCreatedEvent>, std::deque<ObjectiveFinishedEvent>>;
+//     // using SubscriberTypes = std::tuple<SubscribersT<EntityDiedEvent>, SubscribersT<EntityCreatedEvent>, SubscribersT<ObjectiveFinishedEvent>>;
+
+//     using MessageTypes = std::tuple<std::deque<RegisteredTypes>...>;
+//     using SubscriberTypes = std::tuple<SubscribersT<RegisteredTypes>...>;
+
+//     #include <type_traits>
+
+//     template <typename T, typename Tuple>
+//     struct has_type;
+
+//     template <typename T, typename... Us>
+//     struct has_type<T, std::tuple<Us...>> : std::disjunction<std::is_same<T, Us>...>
+//     {
+//     };
+
+// public:
+//     void distributeMessages();
+
+//     template <class MessageDataT>
+//     void send(MessageDataT message);
+
+//     template <class MessageDataT>
+//     void registerEvent();
+
+// private:
+//     template <class MessageData>
+//     friend class PostBox;
+
+//     template <class MessageData>
+//     int subscribeTo(PostOffice::Callback<MessageData>& subscriber);
+
+//     template <class MessageDataT>
+//     void unsubscribe(int id);
+
+//     template <class MessageType>
+//     void distributeToSubscribers(std::deque<MessageType> &messages);
+
+//     template <class MessageDataT>
+//     int findNewFreeId();
+
+//     int next_id = 0;
+
+//     MessageTypes m_message_queues;
+//     SubscriberTypes m_subscribers;
+// };
+
+// using PostOfficeA = PostOffice<EntityDiedEvent, EntityCreatedEvent, ObjectiveFinishedEvent>;
+
+// #include "PostOffice.inl"
--- src/PostOffice.inl
+++ src/PostOffice.inl
@@ -1,60 +1,125 @@
 #pragma once
 
 
+// template <class ...RegisteredTypes>
+// inline void PostOffice<RegisteredTypes...>::distributeMessages()
+// {
+//     std::apply([this](auto&...messages)
+//     { (distributeToSubscribers(messages),...); }, m_message_queues);
+// }
+
+// template <class ...RegisteredTypes>
+// template <class MessageType>
+// inline void PostOffice<RegisteredTypes...>::distributeToSubscribers(std::deque<MessageType> &messages)
+// {
+//     auto &subs = std::get<SubscribersT<MessageType>>(m_subscribers);
+//     for (auto &[id, post_box] : subs)
+//     {
+//         // if(post_box.expired())
+//         // {
+//             // subs.erase(id);
+//         // }else{
+//             post_box(messages);
+//         // }
+//     }
+//     messages.clear();
+// }
+
+// template <class ...RegisteredTypes>
+// template <class MessageDataT>
+// inline void PostOffice<RegisteredTypes...>::send(MessageDataT message)
+// {
+//     std::get<std::deque<MessageDataT>>(m_message_queues).push_back(message);
+// }
+
+// template <class ...RegisteredTypes>
+// template <class MessageData>
+// inline int PostOffice<RegisteredTypes...>::subscribeTo(PostOffice<RegisteredTypes...>::Callback<MessageData>& subscriber)
+// {
+//     int next_id = findNewFreeId<MessageData>();
+//     std::get<SubscribersT<MessageData>>(m_subscribers)[next_id] = subscriber;
+
+//     return next_id;
+// }
+
+// template <class...RegisteredTypes>
+// template <class MessageDataT>
+// inline void PostOffice<RegisteredTypes...>::unsubscribe(int id)
+// {
+//     std::get<SubscribersT<MessageDataT>>(m_subscribers).erase(id);
+// }
+
+// template <class...RegisteredTypes>
+// template <class MessageDataT>
+//  inline int PostOffice<RegisteredTypes...>::findNewFreeId()
+// {
+//     auto &subs = std::get<SubscribersT<MessageDataT>>(m_subscribers);
+//     while (subs.count(next_id) != 0)
+//     {
+//         next_id++;
+//     }
+//     return next_id;
+// }
+
+// template <class...RegisteredTypes>
+// template <class MessageDataT>
+// void PostOffice<RegisteredTypes...>::registerEvent()
+// {
+
+// }
+
+#pragma once
+
 inline void PostOffice::distributeMessages()
 {
-    std::apply([this](auto&...messages)
-               { (distributeToSubscribers(messages),...); }, m_message_queues);
-}
-
-
-template <class MessageType>
-inline void PostOffice::distributeToSubscribers(std::deque<MessageType> &messages)
-{
-    auto &subs = std::get<SubscribersT<MessageType>>(m_subscribers);
-    for (auto &[id, post_box] : subs)
+    for (auto &[type_id, holder] : m_holders)
     {
-        // if(post_box.expired())
-        // {
-            // subs.erase(id);
-        // }else{
-            post_box(messages);
-        // }
+        holder->distribute();
     }
-    messages.clear();
 }
-
 
 
 template <class MessageDataT>
 inline void PostOffice::send(MessageDataT message)
 {
-    std::get<std::deque<MessageDataT>>(m_message_queues).push_back(message);
+    getHolder<MessageDataT>().send(message);
 }
 
-template <class MessageData>
-inline int PostOffice::subscribeTo(PostOffice::Callback<MessageData>& subscriber)
+template <class MessageDataT>
+inline int PostOffice::subscribeTo(Callback<MessageDataT> &subscriber)
 {
-    int next_id = findNewFreeId<MessageData>();
-    std::get<SubscribersT<MessageData>>(m_subscribers)[next_id] = subscriber;
-
-    return next_id;
+    assert(isRegistered<MessageDataT>());
+    auto &holder = getHolder<MessageDataT>();
+    return holder.subscribe(subscriber);
 }
 
+template <class MessageDataT>
+bool PostOffice::isRegistered() const
+{
+    return m_holders.count(std::type_index(typeid(MessageDataT))) > 0;
+}
+
+template <class MessageDataT>
+MessageHolder<MessageDataT> &PostOffice::getHolder()
+{
+    assert(isRegistered<MessageDataT>());
+    return static_cast<MessageHolder<MessageDataT> &>(*m_holders.at(std::type_index(typeid(MessageDataT))));
+}
 template <class MessageDataT>
 inline void PostOffice::unsubscribe(int id)
 {
-    std::get<SubscribersT<MessageDataT>>(m_subscribers).erase(id);
+    getHolder<MessageDataT>().unsubscribe(id);
 }
 
 
 template <class MessageDataT>
- inline int PostOffice::findNewFreeId()
+void PostOffice::registerEvent()
 {
-    auto &subs = std::get<SubscribersT<MessageDataT>>(m_subscribers);
-    while (subs.count(next_id) != 0)
-    {
-        next_id++;
-    }
-    return next_id;
+    m_holders[std::type_index(typeid(MessageDataT))] = std::make_unique<MessageHolder<MessageDataT>>();
+}
+
+template <class...MessageDataT>
+void PostOffice::registerEvents()
+{
+    (registerEvent<MessageDataT>(), ...);
 }
