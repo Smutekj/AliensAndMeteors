19b0b32a9f71778621e331093fb4f60b93994ae6
J S <smutek.jakub@seznam.cz>
2025-07-27 19:13:57 +0200

    collision event

--- src/PostOffice.h
+++ src/PostOffice.h
@@ -8,9 +8,7 @@
 #include <typeindex>
 
 #include "GameEvents.h"
-
-template <class T>
-class PostBox;
+#include "GameObject.h"
 
 using SubscriptionId = int;
 
@@ -91,7 +89,7 @@ public:
     template <class MessageDataT>
     void registerEvent();
 
-    template <class ...MessageData>
+    template <class... MessageData>
     void registerEvents();
 
 private:
@@ -119,59 +117,86 @@ private:
 // using PostOfficeA = PostOffice<EntityDiedEvent, EntityCreatedEvent, ObjectiveFinishedEvent>;
 
 #include "PostOffice.inl"
-// #pragma once
 
-// #include <queue>
-// #include <unordered_map>
-// #include <tuple>
-// #include <memory>
-// #include <functional>
 
-// #include "GameEvents.h"
-
-// template <class T> class PostBox;
-
-// class MessageHolderI
+// struct CollisionEvent2
 // {
-//     // virtual void /
+//     ObjectType type_a;
+//     int id_a;
+//     ObjectType type_b;
+//     int id_b;
 // };
 
-// template <class MessageType>
-// class MessageHolder : public MessageHolderI
+// struct CollisionSubscriptionToken
 // {
-//     std::function<void(const std::deque<MessageType>&)> subscribers;
-//     std::deque<MessageType> messages;
 // };
 
-// class PostOffice
+// class CollisionEventPostOffice
 // {
 
-//     template <class MessageT>
-//     using Callback = std::function<void(const std::deque<MessageT>&)>;
+//     using Callback = std::function<void(CollisionEvent2 &)>;
 //     template <class T>
 //     using SubscribersT = std::unordered_map<int, Callback<T>>;
 
 //     // using MessageTypes = std::tuple<std::deque<EntityDiedEvent>, std::deque<EntityCreatedEvent>, std::deque<ObjectiveFinishedEvent>>;
 //     // using SubscriberTypes = std::tuple<SubscribersT<EntityDiedEvent>, SubscribersT<EntityCreatedEvent>, SubscribersT<ObjectiveFinishedEvent>>;
 
-//     using MessageTypes = std::tuple<std::deque<RegisteredTypes>...>;
-//     using SubscriberTypes = std::tuple<SubscribersT<RegisteredTypes>...>;
-
-//     #include <type_traits>
-
-//     template <typename T, typename Tuple>
-//     struct has_type;
-
-//     template <typename T, typename... Us>
-//     struct has_type<T, std::tuple<Us...>> : std::disjunction<std::is_same<T, Us>...>
-//     {
-//     };
+//     // using MessageTypes = std::tuple<std::deque<RegisteredTypes>...>;
+//     // using SubscriberTypes = std::tuple<SubscribersT<RegisteredTypes>...>;
 
 // public:
-//     void distributeMessages();
+//     void distributeMessages()
+//     {
+//         for (auto &msg : m_to_send)
+//         {
+//             if (type_id2subscribers.contains({msg.type_a, msg.id_b}))
+//             {for (auto &subscriber : type_id2subscribers.at({msg.type_a, msg.id_b}))
+//             {
+//                 subscriber(msg);
+//             }
+//         }
+//             if (type_id2subscribers.contains({msg.type_b, msg.id_a}))
+//             {
+//             for (auto &subscriber : type_id2subscribers.at({msg.type_b, msg.id_a}))
+//             {
+//                 subscriber(msg);
+//             }
+//         }
 
-//     template <class MessageDataT>
-//     void send(MessageDataT message);
+//             if (type_type2subscribers.contains({msg.type_a, msg.type_b}))
+//             {
+//                 for (auto &subscriber : type_type2subscribers.at({msg.type_a, msg.type_b}))
+//                 {
+//                     subscriber(msg);
+//                 }
+//             }
+//         }
+//     }
+
+//     template <class PairIdType>
+//     void distributeToSubscribers(CollisionEvent2 message, PairIdType pair, std::unordered_map<PairIdType, )
+//     {
+//         if()
+//         for (auto &subscriber : id_id2subscribers.at(id_pair))
+//             {
+//                 subscriber(message);
+//             }
+//     }
+
+//     void send(CollisionEvent2 message)
+//     {
+//         std::pair<int, int> id_pair{message.id_a, message.id_b};
+//         if (id_id2subscribers.contains(id_pair))
+//         {
+            
+//         }
+//         if (id_id2subscribers.contains({message.id_b, message.id_b}))
+//         {
+
+//         }
+
+//         m_to_send.push_back(message);
+//     }
 
 //     template <class MessageDataT>
 //     void registerEvent();
@@ -180,22 +205,31 @@ private:
 //     template <class MessageData>
 //     friend class PostBox;
 
-//     template <class MessageData>
-//     int subscribeTo(PostOffice::Callback<MessageData>& subscriber);
+//     int subscribeTo(std::pair<int, int> id_pair, Callback &subscriber)
+//     {
+//         id_id2subscribers[id_pair].push_back(subscriber);
+//     }
+//     int subscribeTo(std::pair<ObjectType, int> type_id_pair, Callback &subscriber)
+//     {
+//         type_id2subscribers[type_id_pair].push_back(subscriber);
+//     }
+//     int subscribeTo(std::pair<ObjectType, ObjectType> type_pair, Callback &subscriber)
+//     {
+//         type_type2subscribers[type_pair].push_back(subscriber);
+//     }
 
-//     template <class MessageDataT>
 //     void unsubscribe(int id);
 
-//     template <class MessageType>
-//     void distributeToSubscribers(std::deque<MessageType> &messages);
-
-//     template <class MessageDataT>
 //     int findNewFreeId();
 
 //     int next_id = 0;
 
-//     MessageTypes m_message_queues;
-//     SubscriberTypes m_subscribers;
+//     std::unordered_map<std::pair<int, int>, std::vector<Callback>> id_id2subscribers;
+//     std::unordered_map<std::pair<ObjectType, int>, std::vector<Callback>> type_id2subscribers;
+//     std::unordered_map<std::pair<ObjectType, ObjectType>, std::vector<Callback>> type_type2subscribers;
+
+//     std::deque<CollisionEvent2> m_to_send;
+//     // SubscriberTypes m_subscribers;
 // };
 
 // using PostOfficeA = PostOffice<EntityDiedEvent, EntityCreatedEvent, ObjectiveFinishedEvent>;
