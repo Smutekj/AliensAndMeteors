aaff0ea845a2a05b3e4c9eede11773b06bd039d0
J S <smutek.jakub@seznam.cz>
2025-08-02 20:41:27 +0200

    more bosses!

---
+++ src/Entities/Bosses.cpp
@@ -0,0 +1,31 @@
+// #include "Bosses.h"
+
+// #include "Triggers.h"
+// #include "GameWorld.h"
+
+// void Boss1::update(float dt)
+// {
+//     if(m_state == State::ShootingLasers)
+//     {
+//         auto& timer = m_world->addObject<Timer>();
+//         timer.m_cooldown = 2.f;
+//         timer.setCallba
+//     }
+
+// }
+// void Boss1::onCreation()
+// {
+
+// }
+// void Boss1::onDestruction()
+// {
+
+// }
+// void Boss1::draw(LayersHolder &target)
+// {
+
+// }
+// void Boss1::onCollisionWith(GameObject &obj, CollisionData &c_data)
+// {
+
+// }
