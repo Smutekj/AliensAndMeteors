44591235789f469464e0f0c57f0b6cf03b299552
J S <smutek.jakub@seznam.cz>
2025-08-02 20:38:15 +0200

    added COmponents And systems

--- src/ComponentSystem.h
+++ src/ComponentSystem.h
@@ -1,248 +1,67 @@
 #pragma once
 
+#include <unordered_map>
+#include <memory>
+#include <typeindex>
+
 #include "Utils/ContiguousColony.h"
 #include "Vector2.h"
 #include "GridNeighbourSearcher.h"
 
-struct BoidComponent
-{
-    utils::Vector2f target_pos;
-    utils::Vector2f pos;
-    utils::Vector2f vel;
-    utils::Vector2f acc; //! resulting acc;
-    float boid_radius = 40.f;
-};
+#include "Systems/System.h"
 
-struct HealthComponent
-{
-    float hp;
-    float max_hp;
-    float hp_regen;
-};
-
-struct ShieldComponent
-{
-    float shield;
-    float max_shield;
-    float shield_regen;
-};
-
-class SystemI
-{
-
-public:
-    virtual void preUpdate(float dt) = 0;
-    virtual void update(float dt) = 0;
-    virtual void postUpdate(float dt) = 0;
-};
-
-template <class ComponentType>
-class System : public SystemI
-{
-public:
-    ComponentType &get(int entity_id)
-    {
-        int comp_id = m_components.id2data_ind.at(entity_id);
-        return m_components.data.at(comp_id);
-    }
-
-    std::vector<ComponentType> &getComponents()
-    {
-        return m_components.data;
-    }
-
-    bool has(int entity_id) const
-    {
-        return m_components.id2data_ind.count(entity_id) > 0;
-    }
-
-    void add(ComponentType comp, int entity_id)
-    {
-        m_components.insert(entity_id, comp);
-    }
-
-    void erase(int entity_id)
-    {
-        m_components.erase(entity_id);
-    }
-
-    virtual void preUpdate(float dt) override {}
-    virtual void postUpdate(float dt) override {}
-    virtual void update(float dt) override
-    {
-        auto comp_count = m_components.data.size();
-        for (std::size_t comp_id = 0; comp_id < comp_count; ++comp_id)
-        {
-            updater(m_components.data[comp_id], dt);
-        }
-    }
-
-private:
-    ContiguousColony<ComponentType, int> m_components;
-
-public:
-    std::function<void(ComponentType &, float)> pre_updater;
-    std::function<void(ComponentType &, float)> updater;
-    std::function<void(ComponentType &, float)> post_updater;
-};
-
-template <>
-class System<BoidComponent> : public SystemI
-{
-    bool has(int entity_id) const
-    {
-        return m_components.contains(entity_id);
-    }
-
-    void add(BoidComponent comp, int entity_id)
-    {
-        m_components.insert(entity_id, comp);
-        m_neighbour_searcher.insertAt(comp.pos, comp, entity_id);
-    }
-
-    void remove(int entity_id)
-    {
-        m_components.erase(entity_id);
-        m_neighbour_searcher.remove(entity_id);
-    }
-
-    virtual void preUpdate(float dt) override
-    {
-        auto comp_count = m_components.data.size();
-        for (std::size_t comp_id = 0; comp_id < comp_count; ++comp_id)
-        {
-            auto &comp = m_components.data[comp_id];
-            m_neighbour_searcher.move(comp.pos, m_components.data_ind2id.at(comp_id));
-        }
-    }
-    virtual void postUpdate(float dt) override
-    {
-    }
-    virtual void update(float dt) override
-    {
-        auto comp_count = m_components.data.size();
-        for (std::size_t comp_id = 0; comp_id < comp_count; ++comp_id)
-        {
-            // updater(m_components.data[comp_id], dt);
-        }
-    }
-
-    void steer(BoidComponent& comp)
-    {
-        auto neighbours2 = m_neighbour_searcher.getNeighbourList(0, comp, comp.boid_radius);
-
-        utils::Vector2f repulsion_force(0, 0);
-        utils::Vector2f push_force(0, 0);
-        utils::Vector2f scatter_force(0, 0);
-        utils::Vector2f cohesion_force(0, 0);
-        utils::Vector2f seek_force(0, 0);
-        float n_neighbours = 0;
-        float n_neighbours_group = 0;
-        utils::Vector2f dr_nearest_neighbours(0, 0);
-        utils::Vector2f average_neighbour_position(0, 0);
-
-        utils::Vector2f align_direction = {0, 0};
-        int align_neighbours_count = 0;
-
-        const float scatter_multiplier = Enemy::m_force_multipliers[Multiplier::SCATTER];
-        const float align_multiplier = Enemy::m_force_multipliers[Multiplier::ALIGN];
-        const float seek_multiplier = Enemy::m_force_multipliers[Multiplier::SEEK];
-
-        auto range_align = std::pow(Enemy::m_force_ranges[Multiplier::ALIGN], 2);
-        auto range_scatter = std::pow(Enemy::m_force_ranges[Multiplier::SCATTER], 2);
-
-        for (auto [neighbour_pos, id] : neighbours2)
-        {
-            // if (p_neighbour == this)
-            
-            // if(ind_j == boid_ind){continue;}
-            const auto dr = neighbour_pos - comp.pos;
-            const auto dist2 = utils::norm2(dr);
-
-            if (dist2 < range_align)
-            {
-                // align_direction += neighbour_boid.m_vel;
-                align_neighbours_count++;
-            }
-
-            if (dist2 < range_scatter)
-            {
-                scatter_force -= scatter_multiplier * dr / dist2;
-                dr_nearest_neighbours += dr / dist2;
-                n_neighbours++;
-            }
-            if (dist2 < range_scatter * 2.f)
-            {
-                average_neighbour_position += dr;
-                n_neighbours_group++;
-            }
-        }
-
-        dr_nearest_neighbours /= n_neighbours;
-
-        if (n_neighbours > 0 && norm2(dr_nearest_neighbours) >= 0.00001f)
-        {
-            scatter_force += -scatter_multiplier * dr_nearest_neighbours / norm(dr_nearest_neighbours) - m_vel;
-        }
-
-        average_neighbour_position /= n_neighbours_group;
-        if (n_neighbours_group > 0)
-        {
-            // cohesion_force =   * average_neighbour_position - m_vel;
-        }
-
-        auto dr_to_target = comp.target_pos - comp.pos;
-        if (norm(dr_to_target) > 3.f)
-        {
-            seek_force = seek_multiplier * max_vel * dr_to_target / norm(dr_to_target) - m_vel;
-        }
-
-        utils::Vector2f align_force = {0, 0};
-        if (align_neighbours_count > 0 && norm2(align_direction) >= 0.001f)
-        {
-            align_force = align_multiplier * align_direction / norm(align_direction) - m_vel;
-        }
-
-        m_acc += (scatter_force + align_force + seek_force + cohesion_force);
-        truncate(m_acc, max_acc);
-    }
-
-private:
-    ContiguousColony<BoidComponent, int> m_components;
-
-    SparseGridNeighbourSearcher<BoidComponent> m_neighbour_searcher;
-
-    std::function<void(BoidComponent &, float)> pre_updater;
-    std::function<void(BoidComponent &, float)> updater;
-    std::function<void(BoidComponent &, float)> post_updater;
-};
+#include "Components.h"
 
 template <class... ComponentTypes>
-class SystemHolder
+class ComponentWorld
 {
 
+    utils::DynamicObjectPool<std::shared_ptr<GameObject>, MAX_ENTITY_COUNT> &m_entity_registry;
+
 public:
+    ComponentWorld(utils::DynamicObjectPool<std::shared_ptr<GameObject>, MAX_ENTITY_COUNT> &entity_registry)
+        : m_entity_registry(entity_registry)
+    {
+    }
+
+    void registerSystem(std::shared_ptr<SystemI> p_system)
+    {
+        m_systems2[std::type_index(typeid(*p_system))] = p_system;
+    }
+
     template <class ComponentType>
     ComponentType &get(int entity_id)
     {
-        std::get<System<ComponentType>>(m_systems).get(entity_id);
+        std::get<ComponentHolder<ComponentType>>(m_components).get(entity_id);
     }
 
     template <class ComponentType>
     bool has(int entity_id) const
     {
-        return std::get<System<ComponentType>>(m_systems).has(entity_id);
+        return std::get<ComponentHolder<ComponentType>>(m_components).has(entity_id);
     }
 
     template <class ComponentType>
-    ComponentType &add(ComponentType comp, int entity_id)
+    void add(ComponentType comp, int entity_id)
     {
-        std::get<System<ComponentType>>(m_systems).add(comp, entity_id);
+        std::get<ComponentHolder<ComponentType>>(m_components).add(comp, entity_id);
     }
+
     template <class ComponentType>
     void remove(int entity_id)
     {
-        std::get<System<ComponentType>>(m_systems).remove(entity_id);
+        std::get<ComponentHolder<ComponentType>>(m_components).erase(entity_id);
+    }
+    void removeEntity(int entity_id)
+    {
+        std::apply([entity_id](auto &&...comp_holder)
+                   { (comp_holder.erase(entity_id), ...); }, m_components);
+    }
+
+    template <class ComponentType>
+    ContiguousColony<ComponentType, int> &getComponents()
+    {
+        return std::get<ComponentHolder<ComponentType>>(m_components).getComponents();
     }
 
     template <class... Components>
@@ -253,22 +72,35 @@ public:
 
     void preUpdate(float dt)
     {
-        std::apply([dt](auto &&...system)
-                   { ((system.preUpdate(dt)), ...); }, m_systems);
+        for (auto &[system_id, system] : m_systems2)
+        {
+            system->preUpdate(dt, m_entity_registry);
+        }
     }
     void update(float dt)
     {
-        std::apply([dt](auto &&...system)
-                   { ((system.update(dt)), ...); }, m_systems);
+        for (auto &[system_id, system] : m_systems2)
+        {
+            system->update(dt);
+        }
     }
+
     void postUpdate(float dt)
     {
-        std::apply([dt](auto &&...system)
-                   { ((system.postUpdate(dt)), ...); }, m_systems);
+        for (auto &[system_id, system] : m_systems2)
+        {
+            system->postUpdate(dt, m_entity_registry);
+        }
     }
 
 private:
-    std::tuple<System<ComponentTypes>...> m_systems;
+    std::unordered_map<std::type_index, std::shared_ptr<SystemI>> m_systems2;
+    std::tuple<ComponentHolder<ComponentTypes>...> m_components;
 };
 
-using GameSystems = SystemHolder<BoidComponent, HealthComponent, ShieldComponent>;
+using GameSystems = ComponentWorld<BoidComponent,
+                                   HealthComponent,
+                                   ShieldComponent,
+                                   AvoidMeteorsComponent,
+                                   TargetComponent,
+                                   TimedEventComponent>;
---
+++ src/Components.h
@@ -0,0 +1,65 @@
+
+#pragma once
+
+#include "Vector2.h"
+#include "Systems/TimedEvent.h"
+
+class GameObject;
+
+struct BoidComponent
+{
+    utils::Vector2f target_pos;
+    utils::Vector2f pos;
+    utils::Vector2f vel = {0,0};
+    utils::Vector2f acc = {0,0}; //! resulting acceleration;
+    float boid_radius = 40.f;
+};
+
+struct TargetComponent
+{
+    GameObject* p_target = nullptr;
+    utils::Vector2f target_pos;
+    float targetting_strength = 1.f;
+};
+
+struct AvoidMeteorsComponent
+{
+    utils::Vector2f target_pos;
+    utils::Vector2f pos;
+    utils::Vector2f vel = {0,0};
+    utils::Vector2f acc = {0,0}; //! resulting acceleration;
+    float radius = 50.f;
+};
+
+struct HealthComponent
+{
+    float hp;
+    float max_hp;
+    float hp_regen;
+};
+
+struct ShieldComponent
+{
+    float shield;
+    float max_shield;
+    float shield_regen;
+};
+
+struct TimedEventComponent
+{
+    int addEvent(TimedEvent& event)
+    {
+        next_id++;
+        events.insert({next_id, event});
+        return next_id;
+    }
+    std::unordered_map<int, TimedEvent> events;
+    int next_id = 0; //! TODO: this is fucked up and will fix it later 
+};
+
+enum class ComponentId
+{
+    Boid,
+    Shield,
+    Hp
+};
