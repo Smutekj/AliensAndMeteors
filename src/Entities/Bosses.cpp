f073289fb918b014a76af6e77b320930a736d525
J S <smutek.jakub@seznam.cz>
2025-08-04 06:51:40 +0200

    added new boss

--- src/Entities/Bosses.cpp
+++ src/Entities/Bosses.cpp
@@ -1,31 +1,71 @@
-// #include "Bosses.h"
+#include "Bosses.h"
 
-// #include "Triggers.h"
-// #include "GameWorld.h"
+#include "../GameWorld.h"
 
-// void Boss1::update(float dt)
-// {
-//     if(m_state == State::ShootingLasers)
-//     {
-//         auto& timer = m_world->addObject<Timer>();
-//         timer.m_cooldown = 2.f;
-//         timer.setCallba
-//     }
+Boss1::Boss1(GameWorld *world, TextureHolder &textures, Collisions::CollisionSystem *collider, PlayerEntity *player)
+: p_player(player), GameObject(world, textures, ObjectType::Boss)
+{
+    m_collision_shape = std::make_unique<Polygon>(4);
+    m_size = {230/2, 398/2};
+    m_collision_shape->setScale(m_size / 2.);
+}
 
-// }
-// void Boss1::onCreation()
-// {
 
-// }
-// void Boss1::onDestruction()
-// {
+void Boss1::update(float dt)
+{
+    //! periodic up-down motion
+    m_motion_time += dt;
+    float motion_frequency = 2.f * 3.14f / m_motion_period;
+    float motion_amplitude = (m_size.y * 0.2f);
+    m_pos.y += dt * motion_amplitude * motion_frequency * std::cos(m_motion_time * motion_frequency);
 
-// }
-// void Boss1::draw(LayersHolder &target)
-// {
 
-// }
-// void Boss1::onCollisionWith(GameObject &obj, CollisionData &c_data)
-// {
+    if (m_state == State::ShootingLasers)
+    {
+        auto shoot_laser_at = [this](utils::Vector2f pos, utils::Vector2f offset)
+        {
+            auto &laser = m_world->addObject2<Laser>();
+            laser.setPosition(pos);
+            setAngle(utils::dir2angle(Vec2{-1.f, 0.f}));
+            laser.setOwner(this);
+            laser.m_offset = offset;
+            laser.m_max_width = 4.;
+            laser.m_max_length = 500.;
+            laser.m_life_time = 3.;
+        };
+        auto shoot_laser_upper = [this, shoot_laser_at]()
+        {
+            shoot_laser_at(m_pos, {0, m_size.y * 0.3});
+        };
+        auto shoot_laser_lower = [this, shoot_laser_at]()
+        {
+            shoot_laser_at(m_pos, {0, - m_size.y * 0.3});
+        };
+        m_vel = {-1, 0};
+        TimedEventComponent timer;
+        timer.addEvent({10.f, shoot_laser_lower, 1});
+        timer.addEvent({5.f, shoot_laser_upper, 1});
+        m_world->m_systems.add(timer, getId());
+        
+        m_state = State::ShootingBigLaser;
+    }
+}
+void Boss1::onCreation()
+{
+}
+void Boss1::onDestruction()
+{
+}
+void Boss1::draw(LayersHolder &target)
+{
+    Sprite ship;
+    ship.setPosition(m_pos);
+    ship.setScale(m_size/2.f);
+    ship.setRotation(utils::to_radains * 90.f);
+    ship.setTexture(*m_textures->get("Boss1"));
 
-// }
+    target.getCanvas("Unit").drawSprite(ship);
+}
+void Boss1::onCollisionWith(GameObject &obj, CollisionData &c_data)
+{
+}
--- src/Entities/Bosses.h
+++ src/Entities/Bosses.h
@@ -1,69 +1,71 @@
-// #pragma once
+#pragma once
 
-// #include "../GameObject.h"
-// #include "Player.h"
+#include "../GameObject.h"
+#include "Player.h"
 
-// class Boss1 : public GameObject
-// {
+class Boss1 : public GameObject
+{
+    enum class State
+    {
+        EnteringFight,
+        ShootingLasers,
+        ShootingGuns,
+        Exposed,
+        ShootingBigLaser,
+    };
+public:
+    Boss1() = default;
+    Boss1(GameWorld *world, TextureHolder &textures, Collisions::CollisionSystem *collider, PlayerEntity *player);
+    Boss1(const Boss1 &e) = default;
+    Boss1 &operator=(Boss1 &e) = default;
+    Boss1 &operator=(Boss1 &&e) = default;
+    virtual ~Boss1() override = default;
 
-//     enum class State
-//     {
-//         EnteringFight,
-//         ShootingLasers,
-//         ShootingGuns,
-//         Exposed,
-//         ShootingBigLaser,
-//     };
+    virtual void update(float dt) override;
+    virtual void onCreation() override;
+    virtual void onDestruction() override;
+    virtual void draw(LayersHolder &target) override;
+    virtual void onCollisionWith(GameObject &obj, CollisionData &c_data) override;
 
+private:
+    void aiWhenRecharged(float dt);
+    void shootAtPlayer();
+    void throwBombsAtPlayer();
+    void shootLasers();
+    void shootLaserAtPlayer();
 
-//     State m_state = State::EnteringFight;
+private:
+    PlayerEntity *p_player;
 
-    
-//     PlayerEntity *m_player;
-//     utils::Vector2f m_acc;
-    
-//     Collisions::CollisionSystem *m_collision_system;
-    
-//     float m_bombing_cooldown = 0.5f;
-//     float m_lasering_cooldown = 3.f;
-    
-//     int m_bomb_count = 0;
-//     float m_shooting_timer = 0.f;
-//     float m_shooting_timer2 = 0.f;
-    
-//     bool m_is_recharging = false;
-//     float m_recharge_time = 7.;
-    
-//     public:
-//     float m_orig_max_vel = 90.f;
-//     float m_max_vel = 90.f;
-//     float max_acc = 130.f;
-//     float m_vision_radius = 70.f;
-    
-//     float m_health = 50;
-//     float m_max_health = 50;
-//     utils::Vector2f m_impulse = {0, 0};
-//     utils::Vector2f m_target_pos;
+    State m_state = State::ShootingLasers;
 
-//     Boss1() = default;
-//     Boss1(GameWorld *world, TextureHolder &textures, Collisions::CollisionSystem *collider = nullptr, PlayerEntity *player = nullptr);
-//     Boss1(const Boss &e) = default;
-//     Boss1 &operator=(Boss1 &e) = default;
-//     Boss1 &operator=(Boss1 &&e) = default;
-//     virtual ~Boss1() override;
+    PlayerEntity *m_player;
+    utils::Vector2f m_acc;
 
-//     virtual void update(float dt) override;
-//     virtual void onCreation() override;
-//     virtual void onDestruction() override;
-//     virtual void draw(LayersHolder &target) override;
-//     virtual void onCollisionWith(GameObject &obj, CollisionData &c_data) override;
+    Collisions::CollisionSystem *m_collision_system;
 
-//     PlayerEntity* p_player
+    float m_motion_time = 0.f;
+    float m_motion_period = 4.f;  
 
-// private:
-//     void aiWhenRecharged(float dt);
-//     void shootAtPlayer();
-//     void throwBombsAtPlayer();
-//     void shootLasers();
-//     void shootLaserAtPlayer();
-// };
+    float m_bombing_cooldown = 0.5f;
+    float m_lasering_cooldown = 3.f;
+
+    int m_bomb_count = 0;
+    float m_shooting_timer = 0.f;
+    float m_shooting_timer2 = 0.f;
+
+    bool m_is_recharging = false;
+    float m_recharge_time = 7.;
+
+public:
+    float m_orig_max_vel = 90.f;
+    float m_max_vel = 90.f;
+    float max_acc = 130.f;
+    float m_vision_radius = 70.f;
+
+    float m_health = 50;
+    float m_max_health = 50;
+    utils::Vector2f m_impulse = {0, 0};
+    utils::Vector2f m_target_pos;
+
+};
