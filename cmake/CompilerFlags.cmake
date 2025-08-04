b490205d7b6e4fe30f978e8992a0538788c502ff
J S <smutek.jakub@seznam.cz>
2025-08-04 06:51:02 +0200

    export flags

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -3,7 +3,8 @@ function(set_target_compiler_flags target_name)
     target_compile_options(${target_name} PRIVATE $<$<CONFIG:Release>:/O2>)
     target_compile_options(${target_name} PRIVATE /MP)
   else()
-    target_compile_options(${target_name} PRIVATE $<$<CONFIG:Release>:-O2>)
+    target_compile_options(${target_name} PRIVATE $<$<CONFIG:Release>:-O3>)
   endif()
+  set(CMAKE_EXPORT_COMPILE_COMMANDS ON) ## to see the compiler flags
 endfunction()
 
