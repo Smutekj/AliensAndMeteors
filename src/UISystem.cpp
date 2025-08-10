81f5e4eabc33fff08af25991c68cfcd85e347a1f
J S <smutek.jakub@seznam.cz>
2025-08-10 08:56:18 +0200

    added boss bar to UISystem

--- src/UISystem.cpp
+++ src/UISystem.cpp
@@ -2,10 +2,24 @@
 #include "Renderer.h"
 #include "Entities/Player.h"
 
-UISystem::UISystem(Renderer &window, TextureHolder &textures, PostOffice &messenger, PlayerEntity *player, Font &font)
-    : ui(window), p_post_office(&messenger), p_player(player), window_canvas(window)
+UISystem::UISystem(Renderer &window, TextureHolder &textures,
+                   PostOffice &messenger, PlayerEntity *player,
+                   Font &font, GameSystems &systems)
+    : ui(window), p_post_office(&messenger),
+      p_player(player), window_canvas(window),
+      m_textures(textures), p_systems(&systems)
 {
+    m_boss_postbox = std::make_unique<PostBox<StartedBossFightEvent>>(messenger, [this](const auto &events)
+                                                                      {
+        for(const auto&  e : events)
+        {
+            boss_id = e.boss_id;
+            addBossBar();
+        } });
+
     auto top_bar = std::make_shared<UIElement>();
+
+    auto bottom_bar = std::make_shared<UIElement>();
     auto fuel_bars = std::make_shared<UIElement>();
     auto health_bars = std::make_shared<UIElement>();
 
@@ -16,9 +30,9 @@ UISystem::UISystem(Renderer &window, TextureHolder &textures, PostOffice &messen
     shield_bar->dimensions = {Percentage{1.f}, Percentage{0.4f}};
     auto money = std::make_shared<TextUIELement>(font, std::to_string(p_player->m_money) + " $");
     money->id = "Money";
-    money->align = Alignement::Center;
+    money->align_x = Alignement::Center;
     money->dimensions = {Pixels{100}, Pixels{30}};
-    
+
     auto fuel_bar = std::make_shared<SpriteUIELement>("fuelBar", textures.get("FireNoise").get());
     auto boost_bar = std::make_shared<SpriteUIELement>("boostBar", textures.get("FireNoise").get());
     fuel_bar->dimensions = {Percentage{1.f}, Percentage{0.4f}};
@@ -26,33 +40,43 @@ UISystem::UISystem(Renderer &window, TextureHolder &textures, PostOffice &messen
 
     fuel_bars->dimensions = {Percentage{0.2f}, Percentage{1.f}};
     fuel_bars->layout = Layout::Y;
-    fuel_bars->align = Alignement::Left;
+    fuel_bars->align_x = Alignement::Left;
     fuel_bars->content_align_x = Alignement::Center;
     fuel_bars->content_align_y = Alignement::Center;
     // fuel_bars->sizing = Sizing::SCALE_TO_FIT;
     fuel_bars->margin.x = {50};
     fuel_bars->id = "FB";
     health_bars->layout = Layout::Y;
-    health_bars->align = Alignement::Right;
+    health_bars->align_x = Alignement::Right;
     health_bars->content_align_x = Alignement::Center;
     health_bars->content_align_y = Alignement::Center;
     health_bars->dimensions = {Percentage{0.2f}, Percentage{1.f}};
     health_bars->margin.x = {50};
     health_bars->id = "HB";
-    
+
     fuel_bars->addChildren(fuel_bar, boost_bar);
     health_bars->addChildren(health_bar, shield_bar);
-    
-    top_bar->dimensions = {Percentage{1.f}, Percentage{0.1f}};
+
+    bottom_bar->dimensions = {Percentage{1.f}, Percentage{0.1f}};
+    bottom_bar->id = "BottomBar";
+    bottom_bar->layout = Layout::X;
+    bottom_bar->align_x = Alignement::CenterX;
+    bottom_bar->content_align_y = Alignement::Center;
+    bottom_bar->addChildren(fuel_bars, money, health_bars);
+
+    top_bar->dimensions = {Percentage{0.8f}, Percentage{0.1f}};
     top_bar->id = "TopBar";
-    top_bar->layout = Layout::X;
-    top_bar->align = Alignement::CenterX;
+    top_bar->layout = Layout::Y;
+    top_bar->align_y = Alignement::Top;
+    top_bar->align_x = Alignement::Center;
     top_bar->content_align_y = Alignement::Center;
-    top_bar->addChildren(fuel_bars, money, health_bars);
+    top_bar->addChildren();
 
     ui.root->dimensions = {Pixels{(float)window.getTargetSize().x}, Pixels{(float)window.getTargetSize().y}};
-    ui.root->addChildren(top_bar);
-    ui.root->content_align_y = Alignement::Top;
+    ui.root->addChildren(top_bar, bottom_bar);
+    ui.root->content_align_y = Alignement::Bottom;
+    ui.root->content_align_x = Alignement::CenterX;
+    ui.root->layout = Layout::Y;
 }
 
 void UISystem::draw(Renderer &window)
@@ -60,9 +84,28 @@ void UISystem::draw(Renderer &window)
     auto old_view = window.m_view;
     window.m_view = window.getDefaultView();
     window.m_view.setSize(window.m_view.getSize().x, -window.m_view.getSize().y);
+    
+    #if DEBUG
+    ui.drawBoundingBoxes(); //! TODO: WHY THE FUCK IS IT FLIPPED WHEN I PUT IT AFTER drawUI???
+    #endif
+    
     ui.drawUI();
 
+
     window.m_view = old_view;
+}
+
+void UISystem::addBossBar()
+{
+    auto boss_bar = std::make_shared<SpriteUIELement>("healthBar", m_textures.get("FireNoise").get());
+    boss_bar->dimensions = {Percentage{1.f}, Percentage{0.4f}};
+    boss_bar->id = "BossHealth";
+    boss_bar->m_shader_id = "bossHealthBar";
+
+    if (auto el = ui.getElementById("TopBar"))
+    {
+        el->addChild(boss_bar);
+    }
 }
 
 void UISystem::update(float dt)
@@ -79,7 +122,18 @@ void UISystem::update(float dt)
     window_canvas.getShader("fuelBar").use();
     window_canvas.getShader("fuelBar").setUniform2("u_fuel_ratio", fuel_ratio);
 
-    float health_ratio = std::min({1.f, p_player->health / p_player->max_health});
+    float health_ratio = std::min({1.f, p_player->getHpRatio()});
     window_canvas.getShader("healthBar").use();
     window_canvas.getShader("healthBar").setUniform2("u_health_ratio", health_ratio);
+
+    if (boss_id != -1)
+    {
+        auto &h_comp = p_systems->get<HealthComponent>(boss_id);
+        float health_ratio = h_comp.hp / h_comp.max_hp;
+
+        window_canvas.getShader("bossHealthBar").use();
+        window_canvas.getShader("bossHealthBar").setUniform2("u_health_ratio", health_ratio);
+    }
+
+    //!
 }
--- src/UISystem.h
+++ src/UISystem.h
@@ -2,23 +2,36 @@
 
 #include "Menu/UIDocument.h"
 #include "PostOffice.h"
+#include "PostBox.h"
+#include "ComponentSystem.h"
 
 class Font;
 
 class UISystem
 {
 public:
-    UISystem(Renderer &window, TextureHolder& textures, PostOffice &messenger, PlayerEntity *player, Font& font);
+    UISystem(Renderer &window, TextureHolder &textures,
+             PostOffice &messenger, PlayerEntity *player,
+             Font &font, GameSystems &systems);
 
     void draw(Renderer &window);
 
     void update(float dt);
 
 private:
+    void addBossBar();
+
+private:
     UIDocument ui;
 
+    GameSystems *p_systems;
     PlayerEntity *p_player;
     PostOffice *p_post_office;
 
-    Renderer& window_canvas;
+    std::unique_ptr<PostBox<StartedBossFightEvent>> m_boss_postbox;
+
+    TextureHolder& m_textures;
+    Renderer &window_canvas;
+
+    int boss_id = -1;
 };
