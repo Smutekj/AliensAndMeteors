a92d1126852285e26ab76d77fa8cab41390d3421
J S <smutek.jakub@seznam.cz>
2025-08-15 18:36:12 +0200

    sprite components and system

---
+++ src/Systems/SpriteSystem.cpp
@@ -0,0 +1,39 @@
+#include "SpriteSystem.h"
+
+
+#include "Renderer.h"
+#include "DrawLayer.h"
+
+SpriteSystem::SpriteSystem(ContiguousColony<SpriteComponent, int> &sprites, LayersHolder& layers)
+: m_components(sprites), m_layers(layers)
+{
+    
+}
+
+void SpriteSystem::preUpdate(float dt, EntityRegistryT &entities)
+{
+    auto &comps = m_components.data;
+    auto &ids = m_components.data_ind2id;
+    for (std::size_t comp_id = 0; comp_id < comps.size(); ++comp_id)
+    {
+        auto &comp = comps[comp_id];
+        comp.sprite.setPosition(entities.at(ids[comp_id])->getPosition());
+        comp.sprite.setRotation(glm::radians(entities.at(ids[comp_id])->getAngle()));
+        comp.sprite.setScale(entities.at(ids[comp_id])->getSize()/2.f);
+    }
+}
+void SpriteSystem::postUpdate(float dt, EntityRegistryT &entities)
+{
+}
+void SpriteSystem::update(float dt)
+{
+    auto &comps = m_components.data;
+    auto &ids = m_components.data_ind2id;
+    for (std::size_t comp_id = 0; comp_id < comps.size(); ++comp_id)
+    {
+        auto &comp = comps[comp_id];
+        auto& canvas = m_layers.getCanvas(comp.layer_id);
+        canvas.drawSprite(comp.sprite, comp.shader_id);
+    }
+
+}
---
+++ src/Systems/SpriteSystem.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "System.h"
+
+class LayersHolder;
+
+
+class SpriteSystem : public SystemI
+{
+public:
+    SpriteSystem(ContiguousColony<SpriteComponent, int> &boids, LayersHolder& layers);
+
+    virtual void preUpdate(float dt, EntityRegistryT &entities) override;
+    virtual void postUpdate(float dt, EntityRegistryT &entities) override;
+    virtual void update(float dt) override;
+
+private:
+    //! I could also store all of the sprite data directly in SpriteBatch to avoid copying
+    //! But it's not a bottleneck right now so who cares?
+    ContiguousColony<SpriteComponent, int> &m_components;
+    LayersHolder& m_layers;
+};
