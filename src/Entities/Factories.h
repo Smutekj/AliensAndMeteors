5c07e75bd37290b49d69ecc1afe7ddb43611d01e
J S <smutek.jakub@seznam.cz>
2025-08-15 18:37:12 +0200

    more factory methods

--- src/Entities/Factories.h
+++ src/Entities/Factories.h
@@ -7,6 +7,7 @@ enum class EnemyType
     LaserEnemy,
     BombEnemy,
     ShooterEnemy,
+    EnergyShooter,
     RocketEnemy
 };
 
@@ -55,26 +56,40 @@ public:
     {
         auto laser_creator = [this, textures](Enemy &enemy) -> Enemy &
         {
-            HealthComponent h_comp = {.hp = 40., .max_hp = 40.};
+            HealthComponent h_comp = {.max_hp = 40.};
             TargetComponent t_comp = {.p_target = m_world.m_player, .targetting_strength = 200.f};
             m_world.m_systems.addEntityDelayed(enemy.getId(), BoidComponent{}, AvoidMeteorsComponent{},
                                                h_comp, t_comp, LaserAIComponent{});
             enemy.m_sprite.setTexture(*textures.get("EnemyLaser"));
+            enemy.m_max_vel = 50;
             return enemy;
         };
         auto shooter_creator = [this, textures](Enemy &enemy) -> Enemy &
         {
-            HealthComponent h_comp = {.hp = 20., .max_hp = 20.};
+            HealthComponent h_comp = { .max_hp = 20.};
             TargetComponent t_comp = {.p_target = m_world.m_player, .targetting_strength = 1000.};
             ShootPlayerAIComponent s_comp = {.cooldown = 3.};
+            SpriteComponent sprite_comp = {.layer_id = "Unit", .sprite = Sprite{*textures.get("EnemyShip")}};
             m_world.m_systems.addEntityDelayed(enemy.getId(), BoidComponent{}, AvoidMeteorsComponent{},
-                                               h_comp, t_comp, s_comp);
-            enemy.m_sprite.setTexture(*textures.get("EnemyShip"));
+                                               h_comp, t_comp, s_comp, sprite_comp);
+            // enemy.m_sprite.setTexture(*textures.get("EnemyShip"));
+            return enemy;
+        };
+        auto energy_shooter_creator = [this, textures](Enemy &enemy) -> Enemy &
+        {
+            HealthComponent h_comp = {.max_hp = 40.};
+            TargetComponent t_comp = {.p_target = m_world.m_player, .targetting_strength = 1000.};
+            ShootPlayerAIComponent s_comp = {.cooldown = 3., .projectile_type = ProjectileType::EnergyBullet};
+            SpriteComponent sprite_comp = {.layer_id = "Unit", .sprite = Sprite{*textures.get("EnemyBomber")}};
+            m_world.m_systems.addEntityDelayed(enemy.getId(), BoidComponent{}, AvoidMeteorsComponent{},
+                                               h_comp, t_comp, s_comp, sprite_comp);
+            enemy.m_max_vel = 50; //! make him slower, lol
             return enemy;
         };
 
         m_creators[EnemyType::LaserEnemy] = laser_creator;
         m_creators[EnemyType::ShooterEnemy] = shooter_creator;
+        m_creators[EnemyType::EnergyShooter] = energy_shooter_creator;
     }
 };
 
@@ -88,25 +103,63 @@ public:
 
     virtual void registerCreators(TextureHolder &textures) override
     {
-        auto fire_bullet_creator = [this](Bullet &bullet) -> Bullet &
+        auto fire_bullet_creator = [this, &textures](Bullet &bullet) -> Bullet &
+        {
+            SpriteComponent s_comp = {.layer_id = "Unit", .shader_id="fireBolt", .sprite = Sprite{*textures.get("EnergyBullet")}};
+            bullet.setBulletType(BulletType::Fire);
+            bullet.setSize({4});
+            bullet.m_max_vel = 300.f;
+            // utils::Vector2f shoot_dir = shooter->m_vel / utils::norm(shooter->m_vel);
+            // bullet.m_vel = shoot_dir * bullet.m_max_vel;
+            // bullet.setPosition(bullet.getPosition() + shoot_dir*5);
+            
+            return bullet;
+        };
+        auto electro_bullet_creator = [this, &textures](Bullet &bullet, GameObject* owner = nullptr) -> Bullet &
+        {
+            SpriteComponent s_comp = {.layer_id = "Unit", .shader_id="lightningBolt", .sprite = Sprite{*textures.get("EnergyBullet")}};
+            bullet.setBulletType(BulletType::Lightning);
+            bullet.setSize({4});
+            bullet.m_max_vel = 300.f;
+            m_world.m_systems.addEntityDelayed(bullet.getId(), s_comp);
+
+            return bullet;
+        };
+        auto energy_bullet_creator = [&textures, this](Bullet &bullet, GameObject* owner = nullptr) -> Bullet &
         {
             bullet.setBulletType(BulletType::Fire);
             bullet.setSize({4});
+            bullet.m_max_vel = 200.f;
+            bullet.m_collision_resolvers[ObjectType::Player] = [](GameObject& obj, CollisionData& c_data)
+            {
+                auto* player = dynamic_cast<PlayerEntity*>(&obj);
+                assert(player);
+                player->m_fuel -= 5.;
+            };
+            SpriteComponent s_comp = {.layer_id = "Unit", .sprite = Sprite{*textures.get("EnergyBullet")}};
+            m_world.m_systems.addEntityDelayed(bullet.getId(), s_comp);
+            
             return bullet;
         };
-
-        auto homing_bullet_creatoer = [this](Bullet &bullet) -> Bullet &
+        
+        auto homing_bullet_creator = [this, &textures](Bullet &bullet, GameObject* owner = nullptr) -> Bullet &
         {
+            SpriteComponent s_comp = {.layer_id = "Unit", .shader_id="fireBolt", .sprite = Sprite{*textures.get("EnergyBullet")}};
+            bullet.setBulletType(BulletType::Fire);
+            bullet.setSize({6});
+
             TargetComponent t_comp = {.p_target = m_world.m_player, .targetting_strength = 100.f};
             m_world.m_systems.addEntityDelayed(bullet.getId(), t_comp);
-            bullet.setSize({6});
+            bullet.m_max_vel = 150.f;
             bullet.m_vel = bullet.m_max_vel * (m_world.m_player->getPosition() - bullet.getPosition());
-            bullet.setBulletType(BulletType::Fire);
             return bullet;
         };
 
         m_creators[ProjectileType::FireBullet] = fire_bullet_creator;
-        m_creators[ProjectileType::HomingFireBullet] = homing_bullet_creatoer;
+        m_creators[ProjectileType::HomingFireBullet] = homing_bullet_creator;
+        m_creators[ProjectileType::EnergyBullet] = energy_bullet_creator;
+        m_creators[ProjectileType::ElectroBullet] = electro_bullet_creator;
+
     }
 };
 
@@ -129,5 +182,61 @@ public:
         };
 
         m_creators[LaserType::Basic] = basic_creator;
+    }
+};
+class PickupFactory : public EntityFactory<PickupFactory, Heart, Pickup>
+{
+public:
+PickupFactory(GameWorld &world, TextureHolder &textures)
+        : EntityFactory<PickupFactory, Heart, Pickup>(world)
+    {
+        registerCreators(textures);
+    }
+
+    virtual void registerCreators(TextureHolder &textures) override
+    {
+
+        m_creators[Pickup::Heart] = [this, &textures](Heart& pickup) -> Heart&
+        {  
+            SpriteComponent sp_comp = {.layer_id = "Unit", .sprite = {*textures.get("Heart")}};
+            m_world.m_systems.addEntityDelayed(pickup.getId(), sp_comp);
+            pickup.m_collision_resolvers[ObjectType::Player] = [this, &pickup](GameObject& obj, auto& c_data)
+            {
+                m_world.m_systems.get<HealthComponent>(obj.getId()).hp += 5;
+                pickup.kill();
+            };
+            return pickup;
+        };
+        m_creators[Pickup::Fuel] = [this, &textures](Heart& pickup) -> Heart&
+        {  
+            SpriteComponent sp_comp = {.layer_id = "Unit", .sprite = {*textures.get("Fuel")}};
+            m_world.m_systems.addEntityDelayed(pickup.getId(), sp_comp);
+            pickup.m_collision_resolvers[ObjectType::Player] = [this, &pickup](GameObject& obj, auto& c_data)
+            {
+                auto p_player = dynamic_cast<PlayerEntity*>(&obj);
+                assert(p_player);
+                p_player->m_fuel = std::min(p_player->m_fuel + 5, p_player->m_max_fuel); 
+                pickup.kill();
+            };
+            pickup.m_collision_resolvers[ObjectType::Bullet] = [this, &pickup](GameObject& obj, auto& c_data)
+            {
+                pickup.kill();
+                obj.kill();
+            };
+            return pickup;
+        };
+        m_creators[Pickup::Money] = [this, &textures](Heart& pickup) -> Heart&
+        {  
+            SpriteComponent sp_comp = {.layer_id = "Unit", .sprite = {*textures.get("Coin")}};
+            m_world.m_systems.addEntityDelayed(pickup.getId(), sp_comp);
+            pickup.m_collision_resolvers[ObjectType::Player] = [this, &pickup](GameObject& obj, auto& c_data)
+            {
+                auto p_player = dynamic_cast<PlayerEntity*>(&obj);
+                assert(p_player);
+                p_player->m_money += 5; 
+                pickup.kill();
+            };
+            return pickup;
+        };
     }
 };
