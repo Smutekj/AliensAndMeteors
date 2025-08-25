3514d87d801596bcd1574a720da62289e9f5a4f6
J S <smutek.jakub@seznam.cz>
2025-08-25 19:11:32 +0200

    removed tinyXML

--- cmake/External.cmake
+++ cmake/External.cmake
@@ -8,14 +8,6 @@ FetchContent_Declare(
 )
 FetchContent_MakeAvailable(renderer)
 
-## tinyXML
-FetchContent_Declare(
-  tinyXML  
-  GIT_REPOSITORY https://github.com/leethomason/tinyxml2
-  GIT_TAG master
-)
-FetchContent_MakeAvailable(tinyXML)
-
 
 FetchContent_Declare(json URL https://github.com/nlohmann/json/releases/download/v3.12.0/json.tar.xz)
 FetchContent_MakeAvailable(json)
