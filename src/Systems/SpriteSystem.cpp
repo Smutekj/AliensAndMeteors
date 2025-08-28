d8ad18d55a4d05779add50eeac1678f8fa7bf6ea
J S <smutek.jakub@seznam.cz>
2025-08-28 08:01:07 +0200

    added ParticleComponent

--- src/Systems/SpriteSystem.cpp
+++ src/Systems/SpriteSystem.cpp
@@ -3,6 +3,7 @@
 
 #include "Renderer.h"
 #include "DrawLayer.h"
+#include "Particles.h"
 
 SpriteSystem::SpriteSystem(ContiguousColony<SpriteComponent, int> &sprites, LayersHolder& layers)
 : m_components(sprites), m_layers(layers)
@@ -37,3 +38,46 @@ void SpriteSystem::update(float dt)
     }
 
 }
+
+
+
+ParticleSystem::ParticleSystem(ContiguousColony<ParticleComponent, int> &comps, LayersHolder& layers)
+: m_components(comps), m_layers(layers)
+{
+    
+}
+
+void ParticleSystem::preUpdate(float dt, EntityRegistryT &entities)
+{
+    auto &comps = m_components.data;
+    auto &ids = m_components.data_ind2id;
+    for (std::size_t comp_id = 0; comp_id < comps.size(); ++comp_id)
+    {
+        auto &comp = comps[comp_id];
+        // comp.sprite.setPosition(entities.at(ids[comp_id])->getPosition());
+        // comp.sprite.setRotation(glm::radians(entities.at(ids[comp_id])->getAngle()));
+        // comp.sprite.setScale(entities.at(ids[comp_id])->getSize()/2.f);
+    }
+}
+void ParticleSystem::postUpdate(float dt, EntityRegistryT &entities)
+{
+    auto &comps = m_components.data;
+    auto &ids = m_components.data_ind2id;
+    for (std::size_t comp_id = 0; comp_id < comps.size(); ++comp_id)
+    {
+        auto &comp = comps[comp_id];
+        auto& canvas = m_layers.getCanvas(comp.layer_id);
+        comp.particles->draw(canvas);
+    }
+}
+void ParticleSystem::update(float dt)
+{
+    auto &comps = m_components.data;
+    auto &ids = m_components.data_ind2id;
+    for (std::size_t comp_id = 0; comp_id < comps.size(); ++comp_id)
+    {
+        auto &comp = comps[comp_id];
+        comp.particles->update(dt);
+    }
+}
+
--- src/Systems/SpriteSystem.h
+++ src/Systems/SpriteSystem.h
@@ -20,3 +20,19 @@ private:
     ContiguousColony<SpriteComponent, int> &m_components;
     LayersHolder& m_layers;
 };
+
+class ParticleSystem : public SystemI
+{
+public:
+ParticleSystem(ContiguousColony<ParticleComponent, int> &comps, LayersHolder& layers);
+
+    virtual void preUpdate(float dt, EntityRegistryT &entities) override;
+    virtual void postUpdate(float dt, EntityRegistryT &entities) override;
+    virtual void update(float dt) override;
+
+private:
+    //! I could also store all of the sprite data directly in SpriteBatch to avoid copying
+    //! But it's not a bottleneck right now so who cares?
+    ContiguousColony<ParticleComponent, int> &m_components;
+    LayersHolder& m_layers;
+};
